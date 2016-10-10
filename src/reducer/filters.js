6b4c035988be57569cf8552a094fffa1995a7270
Roman <kaboom.ua@gmail.com>
2016-10-10 23:39:17 +0300

    Update filters.js

--- src/reducer/filters.js
+++ src/reducer/filters.js
@@ -2,6 +2,7 @@ export default (selectedArticles = null, action) => {
     const { type, payload } = action
     switch(type) {
         case "SELECT_ARTICLE" :
+            //лучше просто хранить id выбранных статей, а присвоение тут совсем лишнее
             return selectedArticles = payload.obj
     }
     return selectedArticles
