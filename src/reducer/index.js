e094737641cc8f851116c962e258b46de30d666a
Roman <kaboom.ua@gmail.com>
2016-10-10 23:40:08 +0300

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import count from './counter'
 import articles from './articles'
 import selected from './filters'
-
+//и опять же даты не хватает
 export default combineReducers({
     articles, count, selected
 })
